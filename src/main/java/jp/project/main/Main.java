13eaf06546eedf00d4be6885ef1eadfbd3105d0f
kirakira <zhuneihui@gmail.com>
2025-04-23 07:30:36 +0900

    flame

--- src/main/java/jp/project/main/Main.java
+++ src/main/java/jp/project/main/Main.java
@@ -1,9 +1,15 @@
 package jp.project.main;
 
+import jp.project.config.AppSettings;
+import jp.project.config.ConfigLoader;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("git test");
-	}
+        AppSettings config = ConfigLoader.load();
+
+        System.out.println("メール: " + config.getEmail());
+        System.out.println("Rakuraku ID: " + config.getRakuraku().getId());
+        System.out.println("Recoru パスワード: " + config.getRecoru().getPassword());	}
 
 }
